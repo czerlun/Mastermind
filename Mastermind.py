671df775e3780927470f3ff0a291ef19e526cd67
czerlun <45076422+czerlun@users.noreply.github.com>
2020-06-27 09:35:38 +0200

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -15,24 +15,14 @@ class Interface(Qwidget):
         self.button3 = QPushButton("reset!")
         self.text_label = QLabel("Witam!")
         self.text_label.setAlignment(Qt.AlignCenter)
-        self.x_1 = QSpinBox(self)
-        self.x_1.setMaximum(6)
-        self.x_1.setMinimum(1)
-        self.x_2 = QSpinBox(self)
-        self.x_2.setMaximum(6)
-        self.x_2.setMinimum(1)
-        self.x_3 = QSpinBox(self)
-        self.x_3.setMaximum(6)
-        self.x_3.setMinimum(1)
-        self.x_4 = QSpinBox(self)
-        self.x_4.setMaximum(6)
-        self.x_4.setMinimum(1)
+        self.spin_box = []
+        for i in range(4):
+            self.spin_box.append(QSpinBox(self))
+            self.spin_box[i].setMaximum(6)
+            self.spin_box[i].setMinimum(1)
+            self.layout.addwidget(self.spin_box[i])
         self.layout = QVBoxLayout()
         self.layout.addwidget(self.text_label)
-        self.layout.addwidget(self.x1)
-        self.layout.addwidget(self.x_2)
-        self.layout.addwidget(self.x_3)
-        self.layout.addwidget(self.x_4)
         self.layout.addwidget(self.button1)
         self.layout.addwidget(self.button2)
         self.layout.addwidget(self.button3)
@@ -59,7 +49,6 @@ class RegulyGry():
         global TURA 
         TURA = 1
         self.tab_results.clear()
-        self.tab_results.clear()
         for _ in range(4):
             self.tab_results.append(random.randint(1, 6))
         if self.tab_results[0] >= 2:
@@ -78,19 +67,12 @@ class oszust_class(RegulyGry):
         global TURA 
         self.my_game.poprawne = random.randint(0, 3)
         self.my_game.wystepujace = random.randint(0, 4)
-
         wynik = []
-        wynik.append(str(self.my_interface.x1.value()))
-        wynik.append(str(self.my_interface.x_2.value()))
-        wynik.append(str(self.my_interface.x_3.value()))
-        wynik.append(str(self.my_interface.x_4.value()))
+        for i in range(4):
+            wynik.append(str(self.my_interface.spin_box[i].value()))
         wynik_w = "".join(wynik)
-
         wynik = f"{TURA} : {wynik_w} Poprawne = {self.my_game.poprawne} Wystepujace = {self.my_game.wystepujace}"
         self.my_interface.text_label.setText(self.my_interface.text_label.text() + '\nTura'+ wynik)
-
-        if self.my_interface.x1.value() == self.my_game.tab_results[0] and self.my_interface.x_2.value() == self.my_game.tab_results[1] and self.my_interface.x_3.value() == self.my_game.tab_results[2] and self.my_interface.x_4.value() == self.my_game.tab_results[3]:
-            self.my_interface.text_label.setText("Wygrana!!!")
         TURA =+ 1
 
     def oszust(self):
@@ -116,47 +98,39 @@ class Logika(RegulyGry):
             self.my_game.wystepujace = 0
             tmp_tab = self.my_game.tab_results[:]
             for i in range(1, 4):
-                tmp = "x"+str(i)
-                self.my_game.tab_user.append(self.my_interface.tmp.value())
-            # self.my_game.tab_user.append(self.my_interface.x1.value())
-            # self.my_game.tab_user.append(self.my_interface.x_2.value())
-            # self.my_game.tab_user.append(self.my_interface.x_3.value())
-            # self.my_game.tab_user.append(self.my_interface.x_4.value())
+                self.my_game.tab_user.append(self.my_interface.spin_box[i].value())
 
-            if self.my_game.tab_results[0] == self.my_interface.x1.value():
+            if self.my_game.tab_results[0] == self.my_interface.spin_box[0].value():
                 self.my_game.poprawne += 1
-            elif self.my_interface.x1.value() in tmp_tab:
+            elif self.my_interface.spin_box[0].value() in tmp_tab:
                 self.my_game.wystepujace += 1
-                tmp_tab.remove(self.my_interface.x1.value())
-            if self.my_game.tab_results[1] == self.my_interface.x_2.value():
+                tmp_tab.remove(self.my_interface.spin_box[0].value())
+            if self.my_game.tab_results[1] == self.my_interface.spin_box[1].value():
                 self.my_game.poprawne += 1
-            elif self.my_interface.x_2.value() in tmp_tab:
+            elif self.my_interface.spin_box[1].value() in tmp_tab:
                 self.my_game.wystepujace += 1
-                tmp_tab.remove(self.my_interface.x_2.value())
-            if self.my_game.tab_results[2] == self.my_interface.x_3.value():
+                tmp_tab.remove(self.my_interface.spin_box[1].value())
+            if self.my_game.tab_results[2] == self.my_interface.spin_box[2].value():
                 self.my_game.poprawne += 1
-            elif self.my_interface.x_3.value() in tmp_tab:
+            elif self.my_interface.spin_box[2].value() in tmp_tab:
                 self.my_game.wystepujace += 1
-                tmp_tab.remove(self.my_interface.x_3.value())
-            if self.my_game.tab_results[3] == self.my_interface.x_4.value():
+                tmp_tab.remove(self.my_interface.spin_box[2].value())
+            if self.my_game.tab_results[3] == self.my_interface.spin_box[3].value():
                 self.my_game.poprawne += 1
-            elif self.my_interface.x_4.value() in tmp_tab:
+            elif self.my_interface.spin_box[3].value() in tmp_tab:
                 self.my_game.wystepujace += 1
-                tmp_tab.remove(self.my_interface.x_4.value())
+                tmp_tab.remove(self.my_interface.spin_box[3].value())
             
             wynik = []
-            wynik.append(str(self.my_interface.x1.value()))
-            wynik.append(str(self.my_interface.x_2.value()))
-            wynik.append(str(self.my_interface.x_3.value()))
-            wynik.append(str(self.my_interface.x_4.value()))
+            for i in range(4):
+                wynik.append(str(self.my_interface.spin_box[i].value()))
             wynik_w = "".join(wynik)
             wynik = f"{TURA} : {wynik_w} Poprawne = {self.my_game.poprawne} Wystepujace = {self.my_game.wystepujace}"
             self.my_interface.text_label.setText(self.my_interface.text_label.text() + '\nTura'+ wynik)
 
-            if ((x.value() for x in self.my_game.tab_user) == self.my_game.tab_results):
+            if [[self.spin_box[x].value() for x in self.my_game.tab_user] == self.my_game.tab_results]:
                 self.my_interface.text_label.setText("Wygrana!!!")
-            #if self.my_interface.x1.value() == self.my_game.tab_results[0] and self.my_interface.x_2.value() == self.my_game.tab_results[1] and self.my_interface.x_3.value() == self.my_game.tab_results[2] and self.my_interface.x_4.value() == self.my_game.tab_results[3]:
-
+            #if self.my_interface.spin_box[0].value() == self.my_game.tab_results[0] and self.my_interface.spin_box[1].value() == self.my_game.tab_results[1] and self.my_interface.spin_box[2].value() == self.my_game.tab_results[2] and self.my_interface.spin_box[3].value() == self.my_game.tab_results[3]:
             TURA += 1
 
     def oszust(self):
