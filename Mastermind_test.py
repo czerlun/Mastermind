d173b01eb6df61a44c61c7fc22a4b0880690939d
czerlun <45076422+czerlun@users.noreply.github.com>
2020-06-30 22:21:26 +0200

    Update Mastermind_test.py

--- Mastermind_test.py
+++ Mastermind_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import unittest
+import Mastermind 
 import random
 import sys
-import Mastermind 
+import unittest
 from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
                                QVBoxLayout, QWidget, QLineEdit, QSpinBox)
 from PySide2.QtCore import Slot, Qt
@@ -12,28 +12,37 @@ class ApplicationTest(unittest.TestCase):
 
     def setUp(self):
         self.game = Mastermind.RegulyGry()
-        self.example_tab = Mastermind.RegulyGry.tab_user = [1,1,1,1]
+        self.example_tab = Mastermind.RegulyGry.tab_user = [1, 1, 1, 1]
 
     def test_trafienia(self):
-        self.good_tab = Mastermind.RegulyGry.tab_results = [1,1,1,1]
+        self.good_tab = Mastermind.RegulyGry.tab_results = [1, 1, 1, 1]
         self.assertEqual(self.example_tab, self.good_tab)
 
     def test_tury(self):
-        Mastermind.TURA = 12
+        Mastermind.TURA = 14
         self.game.Sprawdz()
+        self.assertEqual(f"Porażka , poprawna kombinacja to: {self.my_game.tab_results}", self.my_interface.text_label.text())
 
-    def test_Oszust(self):
+    def test_oszust(self):
         self.game.oszust_flag = 1
         self.game.Oszust()
+        self.assertEqual("Złapałeś/łaś mnie!", self.my_interface.text_label.text())
 
-    def reset(self):
+    def test_reset(self):
         Mastermind.TURA = 10
         self.game.reset()
         self.game.tura += 5
         self.game.Sprawdz()
+        self.assertEqual(1, self.my_game.tura)
 
 
 if __name__ == '__main__':
-    Widget = Interface()
-    Game = RegulyGry(Widget)
+    widget = Mastermind.Interface()
+    game = Mastermind.RegulyGry(Widget)
+    test = ApplicationTest()
+    test.setUp()
+    test.test_trafienia()
+    test.test_tury()
+    test.test_oszust
+    test.test_reset
     unittest.main()
